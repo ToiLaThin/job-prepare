f38518b983be0a2050571b5523baa0c23682ce23
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-08-12 15:21:55 +0700

    review a few more problem with .net

---
+++ LeetCodeC#/containsDuplicate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+bool hasDuplicate(int[] nums) {
+    HashSet<int> set = new HashSet<int>();
+    for(int i = 0; i < nums.Length; i++) {
+        if (set.Contains(nums[i])) {
+            return true;
+        }
+        set.Add(nums[i]);
+    }
+    return false;
+}
+
+int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+System.Console.WriteLine(hasDuplicate(nums));
---
+++ LeetCodeC#/validAnagram/Program.cs
@@ -0,0 +1,41 @@
+﻿// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+
+// Example 2:
+// Input: s = "rat", t = "car"
+// Output: false
+ 
+// Constraints:
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+
+bool IsAnagram(string s, string t) {
+   if (s.Length != t.Length) {
+    return false;
+   }
+
+
+    //use an array to track the count of each character
+    //for s +1, for t -1
+    //if the array is all 0s, then it is an anagram
+    int[] charCount = new int[26];
+
+    for (int i = 0; i < s.Length; i++) {
+        charCount[s[i] - 'a']++;
+        charCount[t[i] - 'a']--;
+    }
+
+    foreach (int count in charCount) {
+        if (count != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+System.Console.WriteLine(IsAnagram("anagram", "nagaram")); // true
+System.Console.WriteLine(IsAnagram("rat", "car")); // false
