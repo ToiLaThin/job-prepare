80f82fb2dcd44a9b12bf2c6acc95fa26764e8d57
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-08-17 06:49:58 +0700

    + solve daily temp(monoStack) & topKFreqElements(bucket)

---
+++ LeetCodeC#/dailyTemparatures/Program.cs
@@ -0,0 +1,69 @@
+﻿// Given an array of integers temperatures represents the daily temperatures, 
+// return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+ 
+
+// Example 1:
+
+// Input: temperatures = [73,74,75,71,69,72,76,73]
+// Output: [1,1,4,2,1,1,0,0]
+// Example 2:
+
+// Input: temperatures = [30,40,50,60]
+// Output: [1,1,1,0]
+// Example 3:
+
+// Input: temperatures = [30,60,90]
+// Output: [1,1,0]
+ 
+
+// Constraints:
+
+// 1 <= temperatures.length <= 105
+// 30 <= temperatures[i] <= 100
+
+int[] dailyTemparatures1(int[] temperatures) {
+    int[] result = new int[temperatures.Length];
+    for(int i = 0; i < temperatures.Length; i++) {
+        for (int j = i + 1; j < temperatures.Length; j++) {
+            if(temperatures[j] > temperatures[i]) {
+                result[i] = j - i;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+// Using Monotonic Stack: 
+// stack will store index and temperatures[i] of previous day
+// for the current day, we will keep popping if curr day > stack top (previous day) and update the result for that previous day
+// finally push the current day to stack
+// Time complexity: O(n)
+// Space complexity: O(n)
+// so the order of the temperatures is decreasing in the stack
+
+// the problem can be also understand as for the current day, find all the nearest previous day with smaller temp and calculate the difference
+// we stop when we find the first previous day with higher temp
+
+// stack when we have debt, we need to pay it back later
+// element in stack are days that not yet found the next nearest day with higher temp in decreasing order
+ int[] dailyTemparatures2(int[] temperatures) {
+    Stack<int> mStackPreviousDayIdx = new Stack<int>();
+    //initialize the result array with 0
+    int[] result = new int[temperatures.Length]; // this is not required as the default value of int is 0
+    //int[] result = new int[temperatures.Length].Select(x => 0).ToArray();
+
+    for(int i = 0; i < temperatures.Length; i++) {
+        while(mStackPreviousDayIdx.Count > 0 && temperatures[i] > temperatures[mStackPreviousDayIdx.Peek()]) {
+            int previousDayIdx = mStackPreviousDayIdx.Pop();
+            result[previousDayIdx] = i - previousDayIdx;
+        }
+        mStackPreviousDayIdx.Push(i);
+    }
+
+    return result;
+}
+
+int[] temperatures = new int[] {73,74,75,71,69,72,76,73};
+Console.WriteLine(string.Join(", ", dailyTemparatures2(temperatures)));
---
+++ LeetCodeC#/topKFreqElements/Program.cs
@@ -0,0 +1,68 @@
+﻿// Given an integer array nums and an integer k, return the k most frequent elements. 
+// You may return the answer in any order.
+
+// Example 1:
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+// k is in the range [1, the number of unique elements in the array].
+// It is guaranteed that the answer is unique.
+ 
+
+// Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+using System;
+using System.Collections.Generic;
+int[] topKFrequent(int[] nums, int k) {
+    Dictionary<int, int> freqCounter = new Dictionary<int, int>();
+    nums.ToList().ForEach(num => {
+        if (freqCounter.ContainsKey(num)) {
+            freqCounter[num]++;
+        } else {
+            freqCounter.Add(num, 1);
+        }
+    });
+
+    // List<List<int>> bucketsByFre = new List<List<int>>();
+    // bucketsByFre must be of size nums.Length + 1 because the maximum frequency of any element can be nums.Length
+    List<int>[] bucketsByFre = new List<int>[nums.Length + 1]; //array of lists (each ele is a list)
+    for (int i = 0; i < bucketsByFre.Length; i++) {
+        bucketsByFre[i] = new List<int>();
+    }
+
+    // sure that entry.Value (frequency) is not greater than nums.Length so we can use it as index of bucketsByFre
+    foreach (var entry in freqCounter) {
+        bucketsByFre[entry.Value].Add(entry.Key);
+    }
+
+    int[] res = new int[k];
+    int resIdx = 0;
+    for (int i = bucketsByFre.Length - 1; i >= 0; i--) {
+        if (bucketsByFre[i].Count <= 0) {
+            continue;
+        }
+        foreach (var ele in bucketsByFre[i]) {
+            res[resIdx] = ele;
+            resIdx++;
+            if (resIdx >=k) {
+                goto end;
+            }
+        }
+    }
+    end:
+        return res;
+}
+
+int[] nums = new int[] {1, 1, 1, 2, 2, 3};
+int k = 2;
+int[] res = topKFrequent(nums, k);
+Console.WriteLine(string.Join(", ", res));
