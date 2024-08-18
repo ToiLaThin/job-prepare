1b1d1eb51f38cfb9a9f74038fbe883a1e9ed585f
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-08-18 20:46:43 +0700

    + solved 2 sliding window & 2 linked list easy problem

---
+++ LeetCodeC#/longestSubStrWithoutRepeatingChar/Program.cs
@@ -0,0 +1,49 @@
+﻿// Given a string s, find the length of the longest 
+// substring without repeating characters.
+
+// Example 1:
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// Example 2:
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+
+// Example 3:
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+// Constraints:
+// 0 <= s.length <= 5 * 104
+// s consists of English letters, digits, symbols and spaces.
+
+int lengthOfLongestSubStr(string s) {
+    //unique char in the current window
+    HashSet<char> charSet = new HashSet<char>();
+    //current window contains unique chars
+    Queue<char> uniqueCharsWindow = new Queue<char>();
+    int maxLen = 0;
+
+    foreach (char c in s) {
+        if (charSet.Contains(c)) {
+            while (uniqueCharsWindow.Count > 0 && uniqueCharsWindow.Peek() != c) {
+                //because substring must be continuous, we can't skip chars in the middle
+                char removedChar = uniqueCharsWindow.Dequeue();
+                charSet.Remove(removedChar);
+            }
+        }
+        charSet.Add(c);
+        uniqueCharsWindow.Enqueue(c);
+        maxLen = Math.Max(maxLen, charSet.Count);
+    }
+
+    return maxLen;
+}
+
+// string s = "abcabcbb";
+string s = "pwwkewabc";
+System.Console.WriteLine(lengthOfLongestSubStr(s)); //3
---
+++ LeetCodeC#/mergeTwoSortedList/Program.cs
@@ -0,0 +1,64 @@
+﻿// You are given the heads of two sorted linked lists list1 and list2.
+
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+// Return the head of the merged linked list.
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Program {
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+
+        if (list2 == null) {
+            return list1;
+        }
+
+        ListNode dummy = new ListNode();
+        ListNode head = list1.val < list2.val ? list1 : list2;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                dummy.next = list1;
+                list1 = list1.next;
+            }
+            else {
+                dummy.next = list2;
+                list2 = list2.next;
+            }
+            dummy = dummy.next;
+        }
+
+        if (list1 == null) {
+            dummy.next = list2;
+        } else {
+            dummy.next = list1;
+        }
+
+        return head;
+    }
+    public static void Main() {
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        // ListNode mergedList = mergeTwoLists(list1, list2);
+        ListNode mergedList = mergeTwoLists(null, new ListNode(0));
+        while (mergedList != null) {
+            System.Console.WriteLine(mergedList.val);
+            mergedList = mergedList.next;
+        }
+    }
+}
