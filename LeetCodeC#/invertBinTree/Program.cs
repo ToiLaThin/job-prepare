69b3dc9759378e73cc453fa24d3a27dfb9e8c673
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-08-19 15:03:09 +0700

    + solved linked list problems (reorder, cycle detect), invert tree, sliding window (pertumation in str)

---
+++ LeetCodeC#/invertBinTree/Program.cs
@@ -0,0 +1,52 @@
+﻿// Given the root of a binary tree, invert the tree, and return its root.
+
+// Input: root = [4,2,7,1,3,6,9]
+// Output: [4,7,2,9,6,3,1]
+
+using System.Diagnostics.Contracts;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public static TreeNode invertBinTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode right = invertBinTree(root.right);
+        TreeNode left = invertBinTree(root.left);
+
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+    public static void Main() {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        TreeNode result = invertBinTree(root);
+        Stack<int> stack = new Stack<int>();
+        while (result != null) {
+            stack.Push(result.val);
+            result = result.left;
+        }
+
+        while (stack.Count > 0) {
+            System.Console.WriteLine(stack.Pop());
+        }
+    }
+}
---
+++ LeetCodeC#/linkedListCycle/Program.cs
@@ -0,0 +1,38 @@
+﻿// Given the head of a singly linked list, reverse the list, and return the reversed list.
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+public class Program {
+    public static bool hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void Main() {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+
+        head.next.next.next.next = head.next;
+
+        System.Console.WriteLine(hasCycle(head));
+    }
+}
