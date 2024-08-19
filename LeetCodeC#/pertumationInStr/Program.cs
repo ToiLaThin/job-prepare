d3fd5e9e817de71cc3d358b70e67ea3a6d86d3b9
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-08-19 15:03:09 +0700

    + solved linked list problems (reorder, cycle detect), invert tree, sliding window (pertumation in str)

---
+++ LeetCodeC#/pertumationInStr/Program.cs
@@ -0,0 +1,74 @@
+﻿// Given two strings s1 and s2, 
+// return true if s2 contains a permutation of s1, or false otherwise.
+// In other words, return true if one of s1's permutations is the substring of s2.
+
+// Example 1:
+// Input: s1 = "ab", s2 = "eidbaooo"
+// Output: true
+// Explanation: s2 contains one permutation of s1 ("ba").
+
+// Example 2:
+// Input: s1 = "ab", s2 = "eidboaoo"
+// Output: false
+ 
+// Constraints:
+// 1 <= s1.length, s2.length <= 104
+// s1 and s2 consist of lowercase English letters.
+
+
+public class Solution {
+    public static bool checkInclusion(string s1, string s2) {
+        int windowSize = s1.Length <= s2.Length ? s1.Length : s2.Length;
+        //init all freq to 0, default value is 0
+        int[] s1Freq = new int[26];
+
+        for (int i = 0; i < s1.Length; i++) {
+            s1Freq[s1[i] - 'a']++;
+        }
+
+        int left = 0;
+        int right = windowSize - 1;
+        while (right < s2.Length) {
+            bool s2HaveCharNotInS1 = false;
+            int[] s2Freq = new int[26];
+            for (int i = left; i <= right; i++) {
+                if (s1Freq[s2[i] - 'a'] == 0) {
+                    s2HaveCharNotInS1 = true;
+                    break;
+                }
+                s2Freq[s2[i] - 'a']++;
+            }
+
+            if (s2HaveCharNotInS1) {
+                left += 1;
+                    right += 1;
+                continue;
+            }
+
+
+            bool isSame = true;
+            for (char i = 'a'; i <= 'z'; i++) {
+                if (s1Freq[i - 'a'] != s2Freq[i - 'a']) {
+                    isSame = false;
+                    break;
+                }
+            }
+
+            if (!isSame) {
+                left += 1;
+                right += 1;
+                continue;
+            }
+            return true;
+            
+
+        }
+        return false;
+    }
+    public static void Main() {
+        string s1 = "ab";
+        // string s2 = "eidbaooo";
+        string s2 = "eidboaoo";
+        System.Console.WriteLine(checkInclusion(s1, s2));
+    }
+}
---
+++ LeetCodeC#/reorderList/Program.cs
@@ -0,0 +1,64 @@
+﻿// You are given the head of a singly linked-list. The list can be represented as:
+
+// L0 → L1 → … → Ln - 1 → Ln
+// Reorder the list to be on the following form:
+
+// L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+// You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+// Input: head = [1,2,3,4,5]
+// Output: [1,5,2,4,3]
+ 
+
+// Constraints:
+// The number of nodes in the list is in the range [1, 5 * 104].
+// 1 <= Node.val <= 1000
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Program {
+    public static void reorderList(ListNode head) {
+        // use list and two pointers to solve the problem
+        // Time complexity: O(n)
+        // Space complexity: O(n)
+        List<ListNode> list = new List<ListNode>();
+        ListNode temp = head;
+        while (temp != null) {
+            list.Add(temp);
+            temp = temp.next;
+        }
+
+        int i = 0, j = list.Count - 1;
+        while (i < j) {
+            list[i].next = list[j];
+            i += 1;
+            if (i == j) {
+                break;
+            }
+            list[j].next = list[i];
+            j -= 1;
+        }
+        list[i].next = null;
+    }
+    public static void Main() {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+
+        reorderList(head);
+        while (head != null) {
+            System.Console.WriteLine(head.val);
+            head = head.next;
+        }
+    }
+}
