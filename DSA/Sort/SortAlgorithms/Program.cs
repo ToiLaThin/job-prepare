182d0b2e6c1abd8dd223226b9b2057ba587d51a5
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-08-27 23:18:47 +0700

    + sort algorithm set up

---
+++ DSA/Sort/SortAlgorithms/Program.cs
@@ -0,0 +1,16 @@
+﻿using DSA.Sort.SortAlgorithms.Abstraction;
+namespace DSA.Sort.SortAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 3, 8, 4, 2 };
+            SortEngineStrategy.Sort(array, new SelectionSortStrategy());
+            foreach (var item in array)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+    }
+}
