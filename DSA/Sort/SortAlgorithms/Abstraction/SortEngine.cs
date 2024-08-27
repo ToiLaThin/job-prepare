f27f456e9182f7f1aa9eed72014e2edb2559791f
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-08-28 06:41:36 +0700

    + delegate - based sort engine and bubble sort implemented

--- DSA/Sort/SortAlgorithms/Abstraction/SortEngine.cs
+++ DSA/Sort/SortAlgorithms/Abstraction/SortEngine.cs
@@ -8,7 +8,43 @@ namespace DSA.Sort.SortAlgorithms.Abstraction
         }
     }
 
-    public interface ISortStrategy {
+    public interface ISortStrategy
+    {
         void Sort(int[] array);
     }
+
+
+    /// <summary>
+    /// represent all method with same signature to be executed when this delegate invoke
+    /// </summary>
+    /// <param name="array"></param>
+    public delegate void SortDelegate(int[] array);
+    public class SortEngineDelegate
+    {
+        private SortDelegate _sortDelegate;
+        public SortEngineDelegate() { }
+        public SortEngineDelegate(SortDelegate sortDelegate)
+        {
+            _sortDelegate = sortDelegate;
+        }
+
+        public void Sort(int[] array)
+        {
+            var sortAlgorithms = _sortDelegate.GetInvocationList();
+            if (sortAlgorithms.Length != 1)
+            {
+                Console.Out.WriteLine("Invalid");
+                return;
+            }
+            _sortDelegate.Invoke(array);
+        }
+
+        //public void SetSortAlgorithm(Action<int[]> sortAlgorithm) { 
+        //    _sortDelegate = new SortDelegate(sortAlgorithm);
+        //}
+
+        public void SetSortAlgorithm(SortDelegate sortDelegate) {
+            _sortDelegate = sortDelegate;
+        }
+    }    
 }
--- DSA/Sort/SortAlgorithms/Program.cs
+++ DSA/Sort/SortAlgorithms/Program.cs
@@ -6,11 +6,56 @@ namespace DSA.Sort.SortAlgorithms
         static void Main(string[] args)
         {
             int[] array = { 5, 3, 8, 4, 2 };
-            SortEngineStrategy.Sort(array, new SelectionSortStrategy());
+            //SortEngineStrategy.Sort(array, new SelectionSortStrategy());
+
+            SortEngineDelegate sortEngineV2 = new SortEngineDelegate();
+            sortEngineV2.SetSortAlgorithm((anyArr) => SelectionSort(anyArr));
+            //signature
+            sortEngineV2.SetSortAlgorithm((anyArr) => BubbleSort(anyArr));
+            sortEngineV2.Sort(array);
+
+            SortDelegate sortSelection = (anyArr) => SelectionSort(anyArr);
             foreach (var item in array)
             {
                 System.Console.WriteLine(item);
             }
+
+        }
+
+        public static void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++) 
+                {
+                    if (array[i] > array[j]) {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void BubbleSort(int[] array)
+        {
+            for (int times = 1; times < array.Length; times++)
+            {
+                for (int j = 0; j < array.Length - times; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void InsertionSort(int[] array)
+        {
+
         }
     }
 }
