f2d0cd5682698900fca96829358d396707b58c89
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-08-28 09:53:08 +0700

    + implement quick sort & merge sort

--- DSA/Sort/SortAlgorithms/Program.cs
+++ DSA/Sort/SortAlgorithms/Program.cs
@@ -1,6 +1,7 @@
-﻿using DSA.Sort.SortAlgorithms.Abstraction;
-namespace DSA.Sort.SortAlgorithms
+﻿namespace DSA.Sort.SortAlgorithms
 {
+    using Abstraction;
+    using Common;
     class Program
     {
         static void Main(string[] args)
@@ -9,52 +10,19 @@ namespace DSA.Sort.SortAlgorithms
             //SortEngineStrategy.Sort(array, new SelectionSortStrategy());
 
             SortEngineDelegate sortEngineV2 = new SortEngineDelegate();
-            sortEngineV2.SetSortAlgorithm((anyArr) => SelectionSort(anyArr));
-            //signature
-            sortEngineV2.SetSortAlgorithm((anyArr) => BubbleSort(anyArr));
+            //sortEngineV2.SetSortAlgorithm((anyArr) => SelectionSort(anyArr));
+            //SortDelegate sortSelection = (anyArr) => SelectionSort(anyArr);
+
+            //sortEngineV2.SetSortAlgorithm((anyArr) => BubbleSort(anyArr));
+
+            //sortEngineV2.SetSortAlgorithm((anyArr) => SortAlgorithms.InsertionSort(anyArr));
+            //sortEngineV2.SetSortAlgorithm(anyArr => SortAlgorithms.MergeSort(anyArr));
+            sortEngineV2.SetSortAlgorithm(anyArr => SortAlgorithms.QuickSort(anyArr));
             sortEngineV2.Sort(array);
 
-            SortDelegate sortSelection = (anyArr) => SelectionSort(anyArr);
-            foreach (var item in array)
-            {
+            foreach (var item in array) {
                 System.Console.WriteLine(item);
             }
-
-        }
-
-        public static void SelectionSort(int[] array)
-        {
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                for (int j = i + 1; j < array.Length; j++) 
-                {
-                    if (array[i] > array[j]) {
-                        int temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }
-                }
-            }
-        }
-
-        public static void BubbleSort(int[] array)
-        {
-            for (int times = 1; times < array.Length; times++)
-            {
-                for (int j = 0; j < array.Length - times; j++)
-                {
-                    if (array[j] > array[j + 1])
-                    {
-                        int temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
-                    }
-                }
-            }
-        }
-
-        public static void InsertionSort(int[] array)
-        {
 
         }
     }
