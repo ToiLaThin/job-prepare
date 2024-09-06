a020e756375af8bfd7c4f8cc9f06579f202b7cad
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-09-06 21:15:30 +0700

    + insert, search in BST, topview bin tree, attemp isSubTreeBinTree

---
+++ DSA/BinarySearchTree/Operations/Program.cs
@@ -0,0 +1,111 @@
+﻿//https://www.geeksforgeeks.org/insertion-in-binary-search-tree/
+
+//Search, Insert, Delete
+//https://www.geeksforgeeks.org/problems/top-view-of-binary-tree/1?ref=gcse_ind
+//https://www.geeksforgeeks.org/print-nodes-top-view-binary-tree/
+using System;
+using System.Collections.Generic;
+
+public class Program {
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        } 
+    }
+
+    public static TreeNode InsertBSTRecursively(TreeNode root, int key) {
+        if (root == null) {
+            return new TreeNode(key);
+        }
+
+        if (root.val == key) {
+            return root;
+        }
+
+        if (key > root.val) {
+            root.right = InsertBSTRecursively(root.right, key);
+        }
+
+        if (key < root.val) {
+            root.left = InsertBSTRecursively(root.left, key);
+        }
+
+        return root;
+    }
+
+    public static TreeNode InsertBSTSlowFast(TreeNode root, int key) {
+        if (root == null) {
+            return new TreeNode(key);
+        }
+
+        if (root.val == key) {
+            return root;
+        }
+
+        TreeNode slow = null;
+        TreeNode fast = root;
+        while (fast != null) {
+            if (key > fast.val) {
+                slow = fast;
+                fast = fast.right;    
+                continue;
+            }
+            if (key < fast.val) {
+                slow = fast;
+                fast = fast.left;
+            }
+        }
+        
+        if (key > slow.val) {
+            slow.right = new TreeNode(key);
+        }
+        else {
+            slow.left = new TreeNode(key);
+        }
+
+        return root;
+    }
+
+    public static TreeNode SearchBSTRecursively(TreeNode root, int key) {
+        if (root == null || root.val == key) {
+            return root;
+        }
+
+        if (key > root.val) {
+            return SearchBSTRecursively(root.right, key);
+        }
+
+        return SearchBSTRecursively(root.left, key);
+    }
+    public static void InOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        InOrder(root.left);
+        System.Console.WriteLine(root.val);
+        InOrder(root.right);
+    }
+    public static void Main(string[] args) {
+        TreeNode root = new TreeNode(1);
+        InsertBSTSlowFast(root, 5);
+        InsertBSTSlowFast(root, 4);
+        InsertBSTSlowFast(root, 3);
+        InsertBSTSlowFast(root, 11);
+
+        InOrder(root);
+
+        TreeNode find = SearchBSTRecursively(root, 100);
+        if (find == null) {
+            System.Console.WriteLine("No item in BST");
+        }
+        else {
+            System.Console.WriteLine(find.val);
+        }
+    }
+}
---
+++ DSA/Tree/View/topViewBinTree/Program.cs
@@ -0,0 +1,72 @@
+﻿//https://www.geeksforgeeks.org/problems/top-view-of-binary-tree/1?ref=gcse_ind
+//https://www.geeksforgeeks.org/print-nodes-top-view-binary-tree/
+using System;
+using System.Collections.Generic;
+
+public class Program {
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        } 
+    }
+
+    public static void topView(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        //nodes visible from above are first nodes with horizontal distance unique
+        //use queue to BFS, calculate hd using a dictionary to store fisrst node with that Horizontal Distance
+        Queue<(TreeNode, int)> queue = new Queue<(TreeNode, int)>();
+        SortedDictionary<int, int> dict = new SortedDictionary<int, int>(); //hold nodes val that visible from top view
+        TreeNode node = null;
+        int nodeHD = 0;
+        queue.Enqueue((root, 0));
+        while (queue.Count > 0) {
+            var tuple = queue.Dequeue();
+            node = tuple.Item1;
+            nodeHD = tuple.Item2;
+
+            if (dict.ContainsKey(nodeHD) == false) {
+                dict.Add(nodeHD, node.val);
+            }
+
+            if (node.left != null) {
+                queue.Enqueue((node.left, nodeHD - 1));
+            }
+
+            if (node.right != null) {
+                queue.Enqueue((node.right, nodeHD + 1));
+            }
+        }
+
+        var nodeKeys = dict.Values.ToList();
+
+        for (int i = 0; i < nodeKeys.Count(); i++) {
+            Console.Write(nodeKeys[i] + " ");
+        }
+    }
+
+    public static void Main(string[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(3);
+        root.left = new TreeNode(2);
+        root.left.right = new TreeNode(5);
+        root.left.left = new TreeNode(4);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+    //      1
+    //   /     \
+    //  2       3
+    // /  \    / \
+    //4    5  6   7
+
+        topView(root);
+    }
+}
