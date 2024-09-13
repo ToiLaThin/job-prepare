e9720b2c53e55e8623a9c333df54ded9bc703489
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-09-13 09:56:13 +0700

    + solved count good node bin tree & validate BST (2 approach)

---
+++ LeetCodeC#/Tree/countGoodNodesBinTree/Program.cs
@@ -0,0 +1,72 @@
+﻿public class Program {
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int GoodNodes(TreeNode root) {
+        int count = 0;
+        //to track max val in the path of root to BEFORE current node
+        Stack<int> maxStack = new Stack<int>();
+        CountGoodNodesDFS(root, ref count, maxStack);        
+        return count;
+    }
+
+    //Time: O(n) n is number of nodes
+    //Space O(h * h) h for stack and h for recursion | why h = log n ? => there is a solution only pass maxVal
+    public static void CountGoodNodesDFS(TreeNode root,ref int count, Stack<int> maxStack) {
+        if (root == null) {
+            return;
+        }
+
+        //curr node is root of whole tree => good node
+        if (maxStack.Count == 0) {
+            count += 1;
+            maxStack.Push(root.val);
+            goto next;
+        }
+        //curr node is  good node
+        int maxInPath = maxStack.Peek();
+        if (root.val >= maxInPath) {
+            count += 1;
+            maxStack.Push(root.val);
+        }
+
+        next:
+        if (root.left != null) {
+            CountGoodNodesDFS(root.left, ref count, maxStack);
+        }
+
+        if (root.right != null) {
+            CountGoodNodesDFS(root.right, ref count, maxStack);
+        }
+
+        //when backtrack, curr node no longer in the path => must remove from max stack if it in maxStack
+        if (root.val == maxStack.Peek()) {
+            maxStack.Pop();
+        }
+    }
+
+    public static void Main(String[] args) {
+        // TreeNode root = new TreeNode(3);
+        // root.left = new TreeNode(1);
+        // root.right = new TreeNode(4);
+        // root.left.left = new TreeNode(3);
+        // root.right.right = new TreeNode(5);
+        // root.right.left = new TreeNode(1);
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(3);
+        root.left.right = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+
+        int countGoodNodes = GoodNodes(root);
+        System.Console.WriteLine(countGoodNodes);
+    }
+}
---
+++ LeetCodeC#/Tree/validateBST/Program.cs
@@ -0,0 +1,89 @@
+﻿// https://leetcode.com/problems/validate-binary-search-tree/description/
+//https://www.enjoyalgorithms.com/blog/validate-binary-search-tree
+public class Program {
+    public class TreeNode {
+        public int val;
+        public TreeNode right;
+        public TreeNode left;
+        public TreeNode(int val = 0, TreeNode right = null, TreeNode left = null) {
+            this.val = val;
+            this.right = right;
+            this.left = left;
+        }
+    }
+
+    //the idea is inorder traversal: we must make sure the value before less than curr value
+    //Time: O(n) n number of node
+    //Space: O(h) h height of tree call stack
+    public static void inOrderValidateBST(TreeNode root, ref TreeNode prevNode,ref bool result) {
+        //do nothing
+        if (root == null) {
+            return;
+        }
+
+        inOrderValidateBST(root.left, ref prevNode, ref result);
+        if (result == false) {
+            return;
+        }
+
+        if (prevNode != null && prevNode.val >= root.val) {
+            result = false;
+            return;
+        }
+
+        //case prev == null => result = true or prev != null and condition match
+        result = true;
+        prevNode = root;
+        inOrderValidateBST(root.right, ref prevNode, ref result);
+        if (result == false) {
+            return;
+        }
+    }
+
+    public static bool isValidBST(TreeNode root) {
+        bool result = true;
+        TreeNode prev = null;
+        inOrderValidateBST(root, ref prev, ref result);
+        return result;
+    }
+
+    public static bool isValidBSTPreorder(TreeNode root) {
+        System.Console.WriteLine(long.MinValue);
+        System.Console.WriteLine(long.MinValue);
+        return preorerValidateBST(root, long.MinValue, long.MaxValue);
+    }
+
+    //Time: O(n) n number of node
+    //Space: O(h) h height of tree call stack
+    public static bool preorerValidateBST(TreeNode root, long left, long right) {
+        if (root == null) {
+            return true;
+        }
+
+        //preorder
+        if (!(root.val > left && root.val < right)) {
+            return false;
+        }
+
+        return preorerValidateBST(root.left, left, root.val) && preorerValidateBST(root.right, root.val, right);
+    }
+
+    public static void Main(string[] args) {
+        // TreeNode root = new TreeNode(2);
+        // root.right = new TreeNode(3);
+        // root.left = new TreeNode(1);
+
+        // TreeNode root = new TreeNode(1);
+        // root.left = new TreeNode(1);
+
+        // TreeNode root = new TreeNode(5);
+        // root.right = new TreeNode(4);
+        // root.left = new TreeNode(1);
+        // root.right.left = new TreeNode(3);
+        // root.right.right = new TreeNode(6);
+
+        TreeNode root = new TreeNode(2147483647);
+
+        System.Console.WriteLine(isValidBSTPreorder(root));
+    }
+}
