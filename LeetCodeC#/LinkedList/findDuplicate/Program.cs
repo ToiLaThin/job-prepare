4cb9cf37059f743dd28ce35426fd72dc3ce60ecb
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-09-21 07:46:15 +0700

    + solve 2 llist question: + copy list with random pointer (using dict to map: oldnode - newNode) + findDuplicate

---
+++ LeetCodeC#/LinkedList/findDuplicate/Program.cs
@@ -0,0 +1,26 @@
+﻿//https://leetcode.com/problems/find-the-duplicate-number/description/
+
+using System;
+using System.Collections.Generic;
+
+//n : 1 - 10^5
+//num[i]: 1 - n
+public class Program {
+    //time: O(n)
+    //space O(n)
+    public static int FindDuplicate(int[] nums) {
+        Dictionary<int, int> counter = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++) {
+            if (counter.ContainsKey(nums[i])) {
+                return nums[i];
+            }
+            counter[nums[i]] = 1;
+        }
+        return 0;
+    }
+    public static void Main(string[] args) {
+        int[] nums = { 1, 3, 4, 2, 2 };
+        int result = FindDuplicate(nums);
+        System.Console.WriteLine(result);
+    }
+}
