ccf33e28183ad30d82ff5b77c82858611058114f
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-10-23 23:47:49 +0700

    + solve search pattern using sliding window, TODO: find bettern approach

---
+++ LeetCodeC#/TwoPointer/searchPatternMantu/Program.cs
@@ -0,0 +1,44 @@
+﻿// input arr: [9, 3, 5, 3, 5, 7, 2, 2]
+// pattern [3, 5, 7] => check if this pattern exist in the arr, return the index
+
+public class Program {
+    public static int searchPattern(int[] arr, int[] pattern) {
+        int p1 = 0; //p for arr
+        int p2 = 0; //p for pattern
+        while (p1 < arr.Length && p2 < pattern.Length) {
+            int pTemp = p1;
+            while (arr[pTemp] == pattern[p2]) {
+                pTemp += 1;
+                p2 += 1;
+
+                if (p2 == pattern.Length) {
+                    return p1;
+                }
+
+                if (pTemp == arr.Length) {
+                    return -1;
+                }
+            }
+            p1 += 1;
+            p2 = 0;
+        }
+
+        //no pattern found
+        if (p1 < arr.Length && p2 == pattern.Length) {
+            return p1;
+        }
+        return -1;
+    }
+    public static void Main(string[] args) {
+        // int[] arr = { 9, 3, 5, 3, 5, 7, 2, 2 };
+        // int[] arr = { 3, 5, 7, 3, 5, 7 };
+        // int[] arr = { 3, 5, 6, 7, 8, 3, 5 };
+        // int[] pattern = { 3, 5, 7};
+
+
+        int[] arr = { 3, 5, 7, 7, 3, 5, 7, 8 };
+        int[] pattern = { 7, 8 };
+        int result = searchPattern(arr, pattern);
+        Console.WriteLine(result);
+    }
+}
